811560e2fb9232f1fba8706a94e89fc3d655bfc8
coolgix <2130891308@qq.com>
2023-10-19 23:20:41 +0800

    day2

--- day1/try.R
+++ day1/try.R
@@ -20,3 +20,29 @@ c(1,3,5)>c(2,4,6)
 a=3+5
 print(a)
 
+#对象名字
+xyz <- 1
+xyz
+hours.per.day <- 24
+hours.per.day
+
+#对象类型
+A <- "hubei University of Medicine"
+A
+
+#逻辑对象
+3*3 == 3^2
+3*2 == 3^2
+
+3*3 == 3^2 & 3*2 == 3^2
+
+#工作空间
+ls()
+
+#查看当前工作目录
+getwd()
+
+#设定当前工作目录
+setwd("D:/LearnR/day2")
+getwd()
+
---
+++ day2/day2.R
@@ -0,0 +1,10 @@
+2+3*5
+x<-4 -pi/2
+x+1
+print(y<- x+1)
+round(y,2)
+
+p <- 0.2
+delta <- p*0.3/2 
+n<- 1.96^2*p*(1-p)/delta^2
+print(n)
